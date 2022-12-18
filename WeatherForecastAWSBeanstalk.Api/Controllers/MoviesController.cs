a6d0289deabc372f10370533233665b8c1bda7b8
JUAN PABLO AGUDELO DIAZ <juanagudelo@innovati.com.co>
2022-12-18 17:46:35 -0500

    Se agrega nuevo endpoint de peliculas

---
+++ WeatherForecastAWSBeanstalk.Api/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecastAWSBeanstalk.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static readonly string[] Movies = new[]
+        {
+            "Avatar", "Batman", "Superman", "Wonder Woman"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> GetMovies()
+        {
+            return Movies;
+        }
+
+        [HttpGet("pelicula/{id}")]
+        public string GetMovieById(int id)
+        {
+            return Movies[id];
+        }
+    }
+}
